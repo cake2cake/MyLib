0514af00a654a86b8add0fca16e0ad6e217db7ec
Cake Cake <joewlfong@gmail.com>
2017-09-07 18:46:31 +0800

    Ebook Apps - First migration with Ebook model

--- MyLib/settings.py
+++ MyLib/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['192.168.0.200',]
 # Application definition
 
 INSTALLED_APPS = [
+    'ebooks.apps.EbooksConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ebooks/models.py
+++ ebooks/models.py
@@ -2,5 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class Ebook(model):
+class Ebook(models.Model):
+    title = models.CharField(max_length=200)
+    publisher = models.CharField(max_length=100)
+    pub_year = models.IntegerField()
+    edition = models.IntegerField()
+    file_name = models.CharField(max_length=200)
+    def __str__(self):
+        return  self.title
+
+
+
+
+
 
